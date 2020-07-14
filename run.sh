1f5c0270fd3eb33b2897fff3e623a10b4645c50c
Scarlet Vidler <scarlet@inspiradigital.co.uk>
2020-07-14 23:37:58 +0100

    downercase on image names

--- run.sh
+++ run.sh
@@ -4,9 +4,9 @@ port=3000
 ip=172.18.0.22
 docker image prune -a -f
 echo "Running on ip $ip and port $port"
-if [ "$(docker ps -q -a -f name=findAGame)" ]; then
-   docker rm -f findAGame
+if [ "$(docker ps -q -a -f name=findagame)" ]; then
+   docker rm -f findagame
 fi
-docker build -t findAGame .
-docker run --restart unless-stopped --log-driver json-file --log-opt max-size=500m --log-opt max-file=2 --name findAGame -p $port:3000 --net inspira --ip $ip --add-host mongo_server:172.18.0.21 -d inspira/findAGame
+docker build -t findagame .
+docker run --restart unless-stopped --log-driver json-file --log-opt max-size=500m --log-opt max-file=2 --name findagame -p $port:3000 --net inspira --ip $ip --add-host mongo_server:172.18.0.21 -d inspira/findagame
 exit $?
