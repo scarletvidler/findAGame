e7dbe1a07c2c5e506454fbc50731f4dd4dbc5c81
Scarlet Vidler <scarlet@inspiradigital.co.uk>
2020-07-14 21:05:56 +0100

    Used prune instead of old system.

--- run.sh
+++ run.sh
@@ -2,12 +2,11 @@ echo "Building docker image for env"
 
 port=3000
 ip=172.18.0.22
-docker rmi $(docker images -q)
+docker image prune -a -f
 echo "Running on ip $ip and port $port"
 if [ "$(docker ps -q -a -f name=findAGame)" ]; then
    docker rm -f findAGame
 fi
 docker build -t findAGame .
-docker run --restart unless-stopped --log-driver json-file --log-opt max-size=500m --log-opt max-file=2 --name findAGame -p $port:3000 --net inspira --ip $ip #--add-host
-mongo_server:172.18.0.21 -d inspira/findAGame
+docker run --restart unless-stopped --log-driver json-file --log-opt max-size=500m --log-opt max-file=2 --name findAGame -p $port:3000 --net inspira --ip $ip --add-host mongo_server:172.18.0.21 -d inspira/findAGame
 exit $?
